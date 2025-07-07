bf47df3fb285a5deb71edd855bbc7758a1403631
siduck <siduck@tutanota.com>
2025-07-07 10:46:15 +0530

    add react example

---
+++ examples/react/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import Button from "haze-ui/button";
+
+function App() {
+  return (
+    <>
+
+      <Button txt="bruh"/>
+    
+      <p className="bg-red">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
