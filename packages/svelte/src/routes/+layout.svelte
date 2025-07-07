a417a4396c0cf7df6371bae8834d1a84f28ef088
siduck <siduck@tutanota.com>
2025-07-07 16:29:09 +0530

    add unocss for svelte dev

---
+++ packages/svelte/src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+import 'virtual:uno.css'
+</script>
+
+<slot/>
--- packages/svelte/src/routes/+page.svelte
+++ packages/svelte/src/routes/+page.svelte
@@ -3,7 +3,7 @@ import  Button from "$lib/components/button.svelte";
    
 </script>
 
-<h1>Welcome to your library project</h1>
+<h1 class='bg-amber'>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
