2f223dc62308bd6d054d07ff45676f7d537a586d
siduck <siduck@tutanota.com>
2025-07-21 15:36:54 +0530

    add type

---
+++ packages/types/button.ts
@@ -0,0 +1,9 @@
+export interface BtnProps {
+  txt?: string;
+  iconL?: string;
+  isLoading?: boolean;
+  loaderTxt?: string;
+  iconR?: string;
+  class?: string;
+  variant?: "primary" | "outline" | "soft" | "ghost";
+}
