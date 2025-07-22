8f3de10fb80fb965d1c5af402761495160049f61
siduck <siduck@tutanota.com>
2025-07-22 10:31:06 +0530

    add @sveltejs/package

---
+++ packages/svelte/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Btn } from "./button.svelte";
+export { default as Dialog } from "./dialog.svelte";
+export { default as Input } from "./input.svelte";
+export { default as Select } from "./select.svelte";
+// export { default as Switch } from "./components/switch";
+// export { default as Userpic } from "./components/userpic";
