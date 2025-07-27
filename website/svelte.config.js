8597162f7bd87d39cfba4e721ae5ccc21bf8147e
siduck <siduck@tutanota.com>
2025-07-27 21:37:16 +0530

    update deploy.yml

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,12 +1,14 @@
 import { mdsvex } from "mdsvex";
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [vitePreprocess(), mdsvex({ extensions: [".mdx"] })],
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      base: "",
+    }),
     alias: {
       "@haze-ui/svelte": "../packages/svelte/src/index",
       "content/*": "./content/*",
