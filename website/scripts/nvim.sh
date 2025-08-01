50096dd554c22ca4b095998e6908fe775bbcaeb4
siduck <siduck@tutanota.com>
2025-08-02 01:12:41 +0530

    misc

--- website/scripts/nvim.sh
+++ website/scripts/nvim.sh
@@ -7,3 +7,28 @@ echo $SHELL
 # Install plugins
 
 log blue "Installing Neovim plugins..."
+
+mkdir -p ~/.local/share/vi/site/pack/plugins/start
+cd ~/.local/share/vi/site/pack/plugins/start
+
+git clone https://github.com/nvim-lua/plenary.nvim --depth 1
+git clone https://github.com/nvim-treesitter/nvim-treesitter --depth 1
+git clone https://github.com/lukas-reineke/indent-blankline.nvim --depth 1
+git clone https://github.com/nvchad/base46 --depth 1
+
+ls -a
+
+log green 'Creating config nvim'
+mkdir ~/.config
+mv nvim ~/.config/nvim
+
+log yellow "Downloading Neovim..."
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
+tar -zxf nvim-linux-x86_64.tar.gz
+mv nvim-linux-x86_64 nv
+
+log green "Compiling base46 themes"
+nv/bin/nvim --headless +"TSUpdate | lua require('base46').compile()" +"q"
+
+log red "Generating svelte components from base46 themes"
+nv/bin/nvim --headless +":lua require 'vihtml'" +"q"
