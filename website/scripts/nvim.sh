fc9e8513bbd20607a65467bcc77f092881c929ba
siduck <siduck@tutanota.com>
2025-08-02 01:51:10 +0530

    -fs

--- website/scripts/nvim.sh
+++ website/scripts/nvim.sh
@@ -2,33 +2,33 @@
 
 . "$(dirname "$0")/globals.sh"
 
-# Install plugins
-#
-
-ls /
-
 cp -r nvim ~/.config/nvim
-ls ~/.config/nvim
-
 
 log blue "Installing Neovim plugins..."
-mkdir -p ~/.local/share/vi/site/pack/plugins/start
 
-# cd ~/.local/share/vi/site/pack/plugins/start
+mkdir -p ~/.local/share/nvim/site/pack/plugins/start
 
-# git clone https://github.com/nvim-lua/plenary.nvim --depth 1
-# git clone https://github.com/nvim-treesitter/nvim-treesitter --depth 1
-# git clone https://github.com/lukas-reineke/indent-blankline.nvim --depth 1
-# git clone https://github.com/nvchad/base46 --depth 1
+cd ~/.local/share/nvim/site/pack/plugins/start
+
+git clone https://github.com/nvim-lua/plenary.nvim --depth 1
+git clone https://github.com/nvim-treesitter/nvim-treesitter --depth 1
+git clone https://github.com/lukas-reineke/indent-blankline.nvim --depth 1
+git clone https://github.com/nvchad/base46 --depth 1
+
+cd 
+
+ls work
+
 
 
 # log yellow "Downloading Neovim..."
-# curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
-# tar -zxf nvim-linux-x86_64.tar.gz
-# mv nvim-linux-x86_64 nv
-#
-# log green "Compiling base46 themes"
-# nv/bin/nvim --headless +"TSUpdate | lua require('base46').compile()" +"q"
-#
-# log red "Generating svelte components from base46 themes"
-# nv/bin/nvim --headless +":lua require 'vihtml'" +"q"
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
+tar -zxf nvim-linux-x86_64.tar.gz
+mv nvim-linux-x86_64 nv
+
+log green "Compiling base46 themes"
+nv/bin/nvim --headless +"lua require('base46').compile()" +"q"
+nv/bin/nvim --headless +"TSUpdate" +"q"
+
+log red "Generating svelte components from base46 themes"
+nv/bin/nvim --headless +":lua require 'vihtml'" +"q"
