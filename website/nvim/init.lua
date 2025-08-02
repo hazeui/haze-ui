5d75ab50a6fe0d10cd5f93277352158f4a0e2e7d
siduck <siduck@tutanota.com>
2025-08-02 23:32:21 +0530

    improve nvim.sh & boostrap plugins automatically

--- website/nvim/init.lua
+++ website/nvim/init.lua
@@ -1,3 +1,6 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46_cache/"
+
+------------------------- options -----------------------
 local o = vim.o
 
 o.shiftwidth = 2
@@ -10,16 +13,57 @@ o.number = true
 o.signcolumn = "yes"
 o.termguicolors = true
 
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46_cache/"
+require "commands"
+
+local plugins_path = vim.fn.stdpath "data" .. "/site/pack/plugins/start/"
+
+local plugins = {
+  "nvim-treesitter/nvim-treesitter",
+  "nvchad/base46",
+  "nvim-lua/plenary.nvim",
+}
+
+local clone_repo = function(repo, branch)
+  local name = repo:match "([^/]+)$"
+  local install_path = plugins_path .. name
+
+  repo = "https://github.com/" .. repo .. ".git"
+
+  local args = { "git", "clone", "--depth", "1", repo, install_path }
+
+  if branch then
+    table.insert(args, "--branch")
+    table.insert(args, branch)
+  end
+
+  vim.fn.system(args)
+
+  vim.notify("installed " .. name)
+end
+
+if not vim.loop.fs_stat(plugins_path) then
+  vim.fn.mkdir(plugins_path, "p")
+
+  for i, repo in ipairs(plugins) do
+    clone_repo(repo, i == 1 and "main" or nil)
+  end
+
+  vim.cmd "packadd nvim-treesitter"
+  vim.cmd "packadd base46"
+  vim.cmd "packadd plenary.nvim"
+
+  vim.cmd "TSInstallParsers"
+
+  require("base46").compile()
+  vim.notify "base46 hlgroups compiled"
+end
 
 require("nvim-treesitter").setup {}
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "*",
   callback = function()
-    pcall(function()
-      vim.treesitter.start()
-    end)
+    pcall(vim.treesitter.start)
   end,
 })
 
@@ -29,4 +73,4 @@ pcall(function()
   dofile(vim.g.base46_cache .. "treesitter")
 end)
 
-require "commands"
+require "globals"
--- website/scripts/nvim.sh
+++ website/scripts/nvim.sh
@@ -2,38 +2,29 @@
 
 . "$(dirname "$0")/globals.sh"
 
+log yellow "Installing tree-sitter CLI..."
+
+curl -LO https://github.com/tree-sitter/tree-sitter/releases/latest/download/tree-sitter-linux-x64.gz
+gunzip tree-sitter-linux-x64.gz
+chmod +x tree-sitter-linux-x64
+sudo mv tree-sitter-linux-x64 /usr/local/bin/tree-sitter
+
+tree-sitter --version
+
 cp -r nvim ~/.config/nvim
 
-log blue "Installing Neovim plugins..."
-
-mkdir -p ~/.local/share/nvim/site/pack/plugins/start
-
-cd ~/.local/share/nvim/site/pack/plugins/start
-
-git clone https://github.com/nvim-lua/plenary.nvim --depth 1
-git clone https://github.com/nvim-treesitter/nvim-treesitter -b main --depth 1
-git clone https://github.com/lukas-reineke/indent-blankline.nvim --depth 1
-git clone https://github.com/nvchad/base46 --depth 1
-
-cd 
+cd
 
 log yellow "Downloading Neovim..."
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
 tar -zxf nvim-linux-x86_64.tar.gz
 
 log blue "Installing Neovim"
-mkdir -p "$HOME/.local/nvim"
-mv nvim-linux-x86_64/* "$HOME/.local/nvim"
+sudo cp -r nvim-linux-x86_64/* /usr/local/
 
-echo "$HOME/.local/nvim/bin" >> "$GITHUB_PATH"
-export PATH="$HOME/.local/nvim/bin:$PATH"
+nvim --headless +"qall"
 
 cd /home/runner/work/haze-ui/haze-ui/website
 
-nvim --headless +"TSInstallParsers" +"TSUpdate" +"q"
-
-log green "Compiling base46 themes"
-nvim --headless +"lua require('base46').compile()" +"q"
-
 log red "Generating svelte components from base46 themes"
-nvim --headless +":lua require 'vihtml'" +"q"
+nvim --headless +":lua require 'vihtml'" +"qall"
